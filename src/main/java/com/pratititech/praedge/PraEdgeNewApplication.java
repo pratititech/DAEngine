fbb03c0b172cd0b40208ec835732f5a126dc9920
nitint <nitint@192.168.3.113>
2019-02-22 09:48:48 +0530

    PraEDge Initial Backend Setup

---
+++ src/main/java/com/pratititech/praedge/PraEdgeNewApplication.java
@@ -0,0 +1,13 @@
+package com.pratititech.praedge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraEdgeNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraEdgeNewApplication.class, args);
+	}
+
+}
