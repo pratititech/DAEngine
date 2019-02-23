d3b465d43d53575980f9ad8f7a51e1d5f1ad29e6
nitint <nitint@nitins-mbp>
2019-02-23 08:38:04 +0530

    OPC UA Client based on Elipse Milo

---
+++ src/main/java/com/pratititech/praedge/opcua/OPCUAClientInterface.java
@@ -0,0 +1,24 @@
+package com.pratititech.praedge.opcua;
+
+import java.util.concurrent.CompletableFuture;
+
+import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
+import org.eclipse.milo.opcua.sdk.client.api.identity.AnonymousProvider;
+import org.eclipse.milo.opcua.sdk.client.api.identity.IdentityProvider;
+import org.eclipse.milo.opcua.stack.core.security.SecurityPolicy;
+
+
+public interface OPCUAClientInterface {
+
+    default SecurityPolicy getSecurityPolicy() {
+        return SecurityPolicy.None;
+    }
+
+    default IdentityProvider getIdentityProvider() {
+        return new AnonymousProvider();
+    }
+
+    void run(OpcUaClient client, CompletableFuture<OpcUaClient> future) throws Exception;
+
+}
+
---
+++ src/main/java/com/pratititech/praedge/opcua/OPCUAClientRunner.java
@@ -0,0 +1,156 @@
+package com.pratititech.praedge.opcua;
+
+import java.io.File;
+import java.security.Security;
+import java.util.Arrays;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.eclipse.milo.opcua.sdk.client.OpcUaClient;
+import org.eclipse.milo.opcua.sdk.client.api.config.OpcUaClientConfig;
+import org.eclipse.milo.opcua.stack.client.UaTcpStackClient;
+import org.eclipse.milo.opcua.stack.core.Stack;
+import org.eclipse.milo.opcua.stack.core.security.SecurityPolicy;
+import org.eclipse.milo.opcua.stack.core.types.builtin.LocalizedText;
+import org.eclipse.milo.opcua.stack.core.types.structured.EndpointDescription;
+import org.eclipse.milo.opcua.stack.core.util.CryptoRestrictions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.eclipse.milo.opcua.stack.core.types.builtin.unsigned.Unsigned.uint;
+
+public class OPCUAClientRunner {
+
+    static {
+        CryptoRestrictions.remove();
+
+        // Required for SecurityPolicy.Aes256_Sha256_RsaPss
+        Security.addProvider(new BouncyCastleProvider());
+    }
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    private final CompletableFuture<OpcUaClient> future = new CompletableFuture<>();
+
+
+    private  final OPCUAClientInterface clientInterface;
+    private final boolean serverRequired;
+
+    public OPCUAClientRunner(OPCUAClientInterface client) throws Exception {
+        this(client, true);
+    }
+
+    public OPCUAClientRunner(OPCUAClientInterface client, boolean serverRequired) throws Exception {
+        this.clientInterface = client;
+        this.serverRequired = serverRequired;
+
+        if (serverRequired) {
+           //TO DO
+        }
+    }
+
+    private OpcUaClient createClient(String getEndpointUrl) throws Exception {
+        File securityTempDir = new File(System.getProperty("java.io.tmpdir"), "security");
+        if (!securityTempDir.exists() && !securityTempDir.mkdirs()) {
+            throw new Exception("unable to create security dir: " + securityTempDir);
+        }
+        LoggerFactory.getLogger(getClass())
+            .info("security temp dir: {}", securityTempDir.getAbsolutePath());
+
+        KeyStoreLoader loader = new KeyStoreLoader().load(securityTempDir);
+
+        SecurityPolicy securityPolicy = clientInterface.getSecurityPolicy();
+
+        EndpointDescription[] endpoints;
+
+        try {
+            endpoints = UaTcpStackClient
+                .getEndpoints(getEndpointUrl)
+                .get();
+        } catch (Throwable ex) {
+            // try the explicit discovery endpoint as well
+            String discoveryUrl = getEndpointUrl + "/discovery";
+            logger.info("Trying explicit discovery URL: {}", discoveryUrl);
+            endpoints = UaTcpStackClient
+                .getEndpoints(discoveryUrl)
+                .get();
+        }
+
+        EndpointDescription endpoint = Arrays.stream(endpoints)
+            .filter(e -> e.getSecurityPolicyUri().equals(securityPolicy.getSecurityPolicyUri()))
+            .findFirst().orElseThrow(() -> new Exception("no desired endpoints returned"));
+
+        logger.info("Using endpoint: {} [{}]", endpoint.getEndpointUrl(), securityPolicy);
+
+        OpcUaClientConfig config = OpcUaClientConfig.builder()
+            .setApplicationName(LocalizedText.english("eclipse milo opc-ua client"))
+            .setApplicationUri("urn:eclipse:milo:examples:client")
+            .setCertificate(loader.getClientCertificate())
+            .setKeyPair(loader.getClientKeyPair())
+            .setEndpoint(endpoint)
+            .setIdentityProvider(clientInterface.getIdentityProvider())
+            .setRequestTimeout(uint(5000))
+            .build();
+
+        return new OpcUaClient(config);
+    }
+
+    public void run(String url) {
+        try {
+            OpcUaClient client = createClient(url);
+
+            future.whenComplete((c, ex) -> {
+                if (ex != null) {
+                    logger.error("Error running example: {}", ex.getMessage(), ex);
+                }
+
+                try {
+                    client.disconnect().get();
+                    //if (serverRequired && exampleServer != null) {
+                        //exampleServer.shutdown().get();
+                    //}
+                    Stack.releaseSharedResources();
+                } catch (InterruptedException | ExecutionException e) {
+                    logger.error("Error disconnecting:", e.getMessage(), e);
+                }
+
+                try {
+                    Thread.sleep(1000);
+                    System.exit(0);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+
+            try {
+                clientInterface.run(client, future);
+                future.get(15, TimeUnit.SECONDS);
+            } catch (Throwable t) {
+                logger.error("Error running client example: {}", t.getMessage(), t);
+                future.completeExceptionally(t);
+            }
+        } catch (Throwable t) {
+            logger.error("Error getting client: {}", t.getMessage(), t);
+
+            future.completeExceptionally(t);
+
+            try {
+                Thread.sleep(1000);
+                System.exit(0);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try {
+            Thread.sleep(999999999);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+
+
